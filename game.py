94a160160a0455b9d189a8ca1590457fbe9c5a1a
tedirland <irlandth@gmail.com>
2023-10-16 17:34:30 -0500

    created Game setings class

--- game.py
+++ game.py
@@ -1,4 +1,15 @@
 from Character import Character
+from GameSettings import GameSettings
 
-the_hero=Character("Ted")
-print(the_hero.character_name)
+character_name = input("Well met, stranger. Welcome to the Drunken Dragon! What be thine name? > ")
+
+the_hero=Character(character_name)
+game_on = True
+
+while game_on:
+    print(f"What can I do fer ye, {the_hero.character_name}?")
+
+    for option in GameSettings.main_options:
+        print(f"{option['input_key']}. {option['text']} ")
+        game_on = False
+
