ec94e97fec37341048e2065f391949fded6252e1
tedirland <irlandth@gmail.com>
2023-10-16 17:23:01 -0500

    Initial commit

---
+++ Character.py
@@ -0,0 +1,3 @@
+class Character():
+    def __init__(self, character_name):
+        self.character_name = character_name
---
+++ game.py
@@ -0,0 +1,4 @@
+from Character import Character
+
+the_hero=Character("Ted")
+print(the_hero.character_name)
